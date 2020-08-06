cc1ffc3f362c6d16fae20b0f19b638c6527b43dd
Geethika Lingamaneni <42781199+GeethikaLingamaneni@users.noreply.github.com>
2020-08-06 12:28:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var myApp=angular.module("myList",[]);
+myApp.controller("myListController",function($scope){
+	$scope.items=[];
+	$scope.newItem="";
+	$scope.pushItem=function()
+	{
+		if($scope.newItem!=""){
+		$scope.items.push($scope.newItem);
+		$scope.newItem="";
+	}
+
+	}
+
+$scope.deleteItem=function(index){
+	$scope.items.splice(index, 1);
+}
+
+
+})
