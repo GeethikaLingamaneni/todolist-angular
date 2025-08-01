9276ec8964171f724bd6f7ed9e349ac60847000b
Geethika Lingamaneni <42781199+GeethikaLingamaneni@users.noreply.github.com>
2025-08-01 17:58:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-var myApp=angular.module("myList",[]);
-myApp.controller("myListController",function($scope){
-	$scope.items=[];
-	$scope.newItem="";
-	$scope.pushItem=function()
-	{
-		if($scope.newItem!=""){
-		$scope.items.push($scope.newItem);
-		$scope.newItem="";
-	}
+var myApp = angular.module("myList", []);
 
-	}
+myApp.controller("myListController", function($scope) {
+    // Load from localStorage
+    $scope.items = JSON.parse(localStorage.getItem("todoList")) || [];
+    $scope.newItem = "";
 
-$scope.deleteItem=function(index){
-	$scope.items.splice(index, 1);
-}
+    // Save to localStorage
+    function saveList() {
+        localStorage.setItem("todoList", JSON.stringify($scope.items));
+    }
 
+    // Add item
+    $scope.pushItem = function() {
+        if ($scope.newItem.trim() !== "") {
+            $scope.items.push({ text: $scope.newItem, done: false });
+            $scope.newItem = "";
+            saveList();
+        }
+    };
 
-})
+    // Delete item
+    $scope.deleteItem = function(index) {
+        $scope.items.splice(index, 1);
+        saveList();
+    };
+
+    // Toggle done status
+    $scope.toggleDone = function(item) {
+        item.done = !item.done;
+        saveList();
+    };
+
+    // Add item when pressing Enter
+    $scope.checkEnter = function(event) {
+        if (event.which === 13) {
+            $scope.pushItem();
+        }
+    };
+});
